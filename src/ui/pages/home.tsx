f41c88c2c0a92a6421981c54c4e93b14e66d6290
Manuel Bento <manuelbentomb.223@hotmail.com>
2025-01-06 17:46:00 +0100

    feat: product list

--- src/ui/pages/home.tsx
+++ src/ui/pages/home.tsx
@@ -1,5 +1,18 @@
+import styles from './home.module.css';
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <section className={styles.products}>
+            {Array.from(Array(4).keys()).map(item=>(
+                <article key={item}>
+                    <div>
+                        <h1>Product {item + 1}</h1>
+                        <h2>$ {Math.floor(Math.random() * 1000)}</h2>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam dignissimos praesentium eius earum quibusdam dolore a saepe, soluta unde rerum dolorem placeat odit? Sint, ad itaque! Cupiditate reprehenderit dicta deleniti.</p>
+                    <button>Buy</button>
+                </article>
+            ))}
+    </section>
   )
 }
