3558d47a7018fa06d30a7a38eab4dd555207af95
Manuel Bento <manuelbentomb.223@hotmail.com>
2025-01-16 09:47:52 +0100

    feat: impl notification and payment service

---
+++ src/infrastructure/services/Notification.ts
@@ -0,0 +1,7 @@
+import { INotificationService } from "@/domain/interfaces/INotificationService";
+
+export class NotificationService implements INotificationService {
+    notify(message: string) {
+        alert(message);
+    }
+}
---
+++ src/infrastructure/services/Payment.ts
@@ -0,0 +1,10 @@
+import { IPaymentService } from "@/domain/interfaces/IPaymentService";
+import { fakeAPI } from "../api";
+
+export class PaymentService implements IPaymentService {
+    async pay(amount: number){
+        console.log(amount);
+        return await fakeAPI(true)
+    }
+    
+}
